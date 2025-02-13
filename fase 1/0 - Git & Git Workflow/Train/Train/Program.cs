84dd7443e5eede967c1198e7131a0e042134b748
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2025-02-13 13:22:56 +0100

    train opdracht

---
+++ fase 1/0 - Git & Git Workflow/Train/Train/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Trainapp
+{
+    class Train
+    {
+        public int aantalWielen;
+        public int aantalPlaatsen;
+        public string conducteur;
+
+        public Train(int wielaantal, int plaatsen, string conducteursnaam)
+        {
+            this.aantalWielen = wielaantal;
+            this.aantalPlaatsen = plaatsen;
+            this.conducteur = conducteursnaam;
+        }
+
+        public int rijden()
+        {
+            return 130;
+        }
+
+        public int remmen()
+        {
+            return 0;
+        }
+    } 
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Train eigenTrain = new Train(7, 30, "Eugene");
+
+            Train nieuweTrain = new Train(20, 29, "Sinterklaas");
+            Console.WriteLine(eigenTrain.aantalWielen);
+            Console.WriteLine(nieuweTrain.aantalWielen);
+
+        }
+    }
+}
